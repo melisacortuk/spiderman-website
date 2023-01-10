05fd7939b4459eb293a25608e9949747fc1d2226
Melisa Çörtük <76431780+melisacortuk@users.noreply.github.com>
2023-01-10 21:27:13 +0300

    Add files via upload

---
+++ spiderman-website/assets/js/main.js
@@ -0,0 +1,6 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.querySelector('.nav-list');
+/*===== MENU SHOW ====*/
+document.querySelector('.nav-toggle').onclick = () => {
+  navMenu.classList.toggle('show-menu');
+}
