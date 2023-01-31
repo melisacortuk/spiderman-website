5f582114edc5dc613de71fb11e48ba3face4a42d
Melisa Çörtük <76431780+melisacortuk@users.noreply.github.com>
2023-01-31 16:54:19 +0300

    Delete spiderman-website directory

--- spiderman-website/assets/js/main.js
+++
@@ -1,6 +0,0 @@
-/*=============== SHOW MENU ===============*/
-const navMenu = document.querySelector('.nav-list');
-/*===== MENU SHOW ====*/
-document.querySelector('.nav-toggle').onclick = () => {
-  navMenu.classList.toggle('show-menu');
-}
